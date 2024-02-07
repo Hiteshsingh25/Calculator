fce1d9b0a28de33b2545da909ef465cc5c0007fb
Hiteshsingh25 <shekhawathitesh7@gmail.com>
2024-02-07 12:45:16 +0530

    third commit

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -17,22 +17,28 @@ const initialCal = {
 export function Calculation(state = initialCal, action) {
   switch (action.type) {
     case EQUATE_NUM:
-      console.log(state.num1)
-        if (String(state.num1).includes(".") && action.payload === '.' && String(state.num2).includes('.') && action.payload === ('.')) {
+      // console.log(state.num1)
+      if (String(state.num2).includes(".") && action.payload === '.' ) {
+        console.log('num2 ka .')
+        console.log(state)
+        return state
+    }else if (state.num1 !== "" && state.sign !== "" ) {
+        return { ...state, num2: state.num2 + action.payload };
+      }  
+     
+        if (String(state.num1).includes(".") && action.payload === '.' ) {
             console.log('num1 ka .')
             console.log(state)
             return state
         }
-        
-         if (state.num1 !== "" && state.sign !== "") {
-        return { ...state, num2: state.num2 + action.payload };
-      } else {
+       
+      else {
         return {
           ...state,
           num1: state.num1 + action.payload,
         };
       }
-     
+        
     case SIGN:
       if (state.sign !== "") {
         return { ...state, sign: state.sign };
@@ -86,14 +92,14 @@ export function Calculation(state = initialCal, action) {
         return state
       }
       if (state.num2) {
-        const preNum = state.num2;
+        const preNum = state?.num2;
         const sts =preNum.slice(0,-1);
         console.log('num2',{sts})
         return {
           ...state,
           num2: sts,
         };
-      } else if (state.sign && !state.num2) {
+      } else if (state?.sign && !state.num2) {
         console.log("state.sign",state.sign);
 
         const preNum = state.sign;
@@ -107,8 +113,9 @@ export function Calculation(state = initialCal, action) {
       } else if (state.num1) {
         console.log("state.num1",state.num1);
 
-        const preNum = state.num1;
-        const numsts  = preNum.slice(0, -1);
+        const preNum = String(state?.num1);
+        console.log({preNum})
+        const numsts  = preNum?.slice(0, -1);
         console.log('num1',{numsts},{preNum})
         return {
           ...state,
